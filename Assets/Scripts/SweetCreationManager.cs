9fd98d16fd58b0ffa0ad31e21707a73752e404fe
Angela C - Lab101 <79293475+Ay3e@users.noreply.github.com>
2023-06-29 15:52:00 +1000

    Added a toppings limit, refresh, and recipe book

--- Assets/Scripts/SweetCreationManager.cs
+++ Assets/Scripts/SweetCreationManager.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEditor.Build.Content;
 
 public class SweetCreationManager : MonoBehaviour
 {
@@ -9,128 +13,189 @@ public class SweetCreationManager : MonoBehaviour
     //arrays
     [SerializeField] private GameObject[] baseSprites;
     [SerializeField] private GameObject[] ingredientSprites;
-    [SerializeField] private GameObject[] displayedIngredients;
+    [SerializeField] private GameObject[] recipeBookBaseSprites;
 
     //other
-    [SerializeField] private GameObject IngredientTypes;
-    private GameObject selectedBase;
-    private GameObject pressedIngredient;
-    //private int ingredientCounter;
+    [SerializeField] private GameObject ingredientTypes;
+    [SerializeField] private GameObject recipePanel;
+    [SerializeField] private GameObject nextButton;
+    [SerializeField] private GameObject finishButton;
+    [SerializeField] private TextMeshProUGUI toppingsLeftCounter;
+    private GameObject _selectedBase;
+    private GameObject _pressedIngredient;
+    
+    private int _maxTopping = 3;
 
-    private int creamCount;
-    private int berryCount;
-    private int sugarCount;
-    private int chocolateCount;
-
+    private int _creamCount;
+    private int _berryCount;
+    private int _sugarCount;
+    private int _chocolateCount;
 
     private void Start()
     {
         //sets base panel to always be active first
+        _selectedBase = baseSprites[0];
         basePanel.SetActive(true);
         ingredientsPanel.SetActive(false);
-        IngredientTypes.SetActive(false);
+        ingredientTypes.SetActive(false);
+        nextButton.SetActive(true);
+        finishButton.SetActive(false);
     }
 
     private void Update()
     {
-        //
-        if (selectedBase == null)
+        for (int i = 0; i < baseSprites.Length; i++)
         {
-            selectedBase = baseSprites[0];
-            selectedBase.SetActive(true);
-        }
-        else
-        {
-            for (int i = 0; i < baseSprites.Length; i++)
+            if (_selectedBase == baseSprites[i])
             {
-                if (selectedBase == baseSprites[i])
-                {
-                    selectedBase.SetActive(true);
-                }
-                else
-                {
-                    baseSprites[i].SetActive(false);
-                }
+                _selectedBase.SetActive(true);
             }
-            for (int i = 0; i < ingredientSprites.Length; i++)
+            else
             {
-                if (pressedIngredient == ingredientSprites[i])
-                {
-                    pressedIngredient.SetActive(true);
-                }
+                baseSprites[i].SetActive(false);
             }
         }
+        for (int i = 0; i < ingredientSprites.Length; i++)
+        {
+            if (_pressedIngredient == ingredientSprites[i])
+            {
+                _pressedIngredient.SetActive(true);
+            }
+        }
+        if (_maxTopping <= 0)
+        {
+            _pressedIngredient = null;
+            _maxTopping = 0;
+        }
+        toppingsLeftCounter.text = "TOPPINGS LEFT " + _maxTopping;
+        ChangeBaseToDiscovered();
     }
     //NEXT BUTTON
-    public void nextButton()
+    public void NextButton()
     {
         basePanel.SetActive(false);
         ingredientsPanel.SetActive(true);
-        IngredientTypes.SetActive(true);
+        ingredientTypes.SetActive(true);
+        nextButton.SetActive(false);
+        finishButton.SetActive(true);
+    }
+
+    //FINISH BUTTON
+    public void FinishButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //RECIPE BOOK BUTTON
+    public void RecipeBookButton()
+    {
+        recipePanel.SetActive(!recipePanel.activeSelf);
     }
 
     //BASE BUTTONS
     public void SpongeBaseButton()
     {
-        selectedBase = baseSprites[0];
+        _selectedBase = baseSprites[0];
     }
     public void JellyBaseButton()
     {
-        selectedBase = baseSprites[1];
+        _selectedBase = baseSprites[1];
+    }
+
+    //DISCOVER NEW BASES
+    private void ChangeBaseToDiscovered()
+    {
+        //Sponge cake to Short cake
+        if(_selectedBase == baseSprites[0] && _creamCount>= 1 && _berryCount >= 1)
+        {
+            _selectedBase = baseSprites[2];
+            recipeBookBaseSprites[2].GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        //Sponge cake to Cream puff
+        if (_selectedBase == baseSprites[0] && _creamCount >= 1 && _sugarCount >= 1)
+        {
+            _selectedBase = baseSprites[3];
+            recipeBookBaseSprites[3].GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        //Sponge cake to Chocolate cake
+        if (_selectedBase == baseSprites[0] && _chocolateCount >=2)
+        {
+            _selectedBase = baseSprites[4];
+            recipeBookBaseSprites[4].GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        //Jelly to Pudding
+        if (_selectedBase == baseSprites[1] && _sugarCount>=1 && _creamCount>=1)
+        {
+            _selectedBase = baseSprites[5];
+            recipeBookBaseSprites[5].GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        //Jelly to Ice Cream
+        if (_selectedBase == baseSprites[1] && _creamCount >= 2)
+        {
+            _selectedBase = baseSprites[6];
+            recipeBookBaseSprites[6].GetComponent<SpriteRenderer>().color = Color.white;
+        }
     }
 
     //INGREDIENT BUTTONS
     public void BigBerryButton()
     {
-        pressedIngredient = ingredientSprites[0];
+        _pressedIngredient = ingredientSprites[0];
         InstantiatePressedIngredient();
-        berryCount++;
+        _berryCount++;
+        _maxTopping--;
     }
     public void ChocoStickButton()
     {
-        pressedIngredient = ingredientSprites[1];
+        _pressedIngredient = ingredientSprites[1];
         InstantiatePressedIngredient();
-        chocolateCount++;
+        _chocolateCount++;
+        _maxTopping--;
     }
     public void IceCreamButton()
     {
-        pressedIngredient = ingredientSprites[2];
+        _pressedIngredient = ingredientSprites[2];
         InstantiatePressedIngredient();
-        creamCount++;
+        _creamCount++;
+        _maxTopping--;
     }
     public void BlueberriesButton()
     {
-        pressedIngredient = ingredientSprites[3];
+        _pressedIngredient = ingredientSprites[3];
         InstantiatePressedIngredient();
+        _maxTopping--;
     }
     public void ChocoLogoButton()
     {
-        pressedIngredient = ingredientSprites[4];
+        _pressedIngredient = ingredientSprites[4];
         InstantiatePressedIngredient();
-        chocolateCount++;
+        _chocolateCount++;
+        _maxTopping--;
     }
     public void TripleBerriesButton()
     {
-        pressedIngredient = ingredientSprites[5];
+        _pressedIngredient = ingredientSprites[5];
         InstantiatePressedIngredient();
-        berryCount++;
+        _berryCount++;
+        _maxTopping--;
     }
     public void SoyFlourButton()
     {
-        pressedIngredient = ingredientSprites[6];
+        _pressedIngredient = ingredientSprites[6];
         InstantiatePressedIngredient();
+        _maxTopping--;
     }
 
-    // Dupilcate the ingredient
+    // DUPLICATE THE INGREDIENTS
     private void InstantiatePressedIngredient()
     {
-        if (pressedIngredient != null)
+        if (_maxTopping <= 0)
         {
-            GameObject instantiatedIngredient = Instantiate(pressedIngredient, pressedIngredient.transform.position, pressedIngredient.transform.rotation);
-            // You can perform additional operations on the instantiated ingredient
-            Rigidbody instantiatedRigidbody = instantiatedIngredient.AddComponent<Rigidbody>();
-            instantiatedRigidbody.mass = 1f;
-            instantiatedRigidbody.drag = 0.5f;
+            _pressedIngredient = null;
+        }
+        if (_pressedIngredient != null)
+        {
+            GameObject instantiatedIngredient = Instantiate(_pressedIngredient, _pressedIngredient.transform.position, _pressedIngredient.transform.rotation);
         }
     }
 }
